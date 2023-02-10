d4d84116061786560011827393b9e4511a545186
ily123 <ibnovikov@gmail.com>
2023-02-09 22:42:16 -0600

    Add basic Tailwind styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
+  <main className="flex justify-center pt-10">
+    <h1 className="text-4xl max-sm:text-3xl font-light font-mono text-gray-100">
+      NBA Allstar 2023
     </h1>
-  )  
+  </main>
+  )
 
 }
 
